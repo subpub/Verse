e0c318e76760c5bfe0fdb242363c974589e43845
Matthew Wild <mwild1@gmail.com>
2009-11-28 22:24:20 +0000

    util.logger: Friendlier string.format to automatically tostring() arguments

--- libs/logger.lua
+++ libs/logger.lua
@@ -1,9 +1,21 @@
 local print = print
+local select, tostring = select, tostring;
 module "logger"
 
+local function format(format, ...)
+	local n, maxn = 0, #arg;
+	return (format:gsub("%%(.)", function (c) if c ~= "%" and n <= maxn then n = n + 1; return tostring(arg[n]); end end));
+end
+
+local function format(format, ...)
+	local n, maxn = 0, select('#', ...);
+	local arg = { ... };
+	return (format:gsub("%%(.)", function (c) if n <= maxn then n = n + 1; return tostring(arg[n]); end end));
+end
+
 function init(name)
-	return function (level, message)
-		print(level, message);
+	return function (level, message, ...)
+		print(level, format(message, ...));
 	end
 end
 
