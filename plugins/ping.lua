b7ecbb8a4f47a1884428f63f58177c3fb8ff0ec2
Matthew Wild <mwild1@gmail.com>
2009-12-20 21:33:55 +0000

    plugins.ping: Add ping plugin to XMPP ping a JID

---
+++ plugins/ping.lua
@@ -0,0 +1,10 @@
+
+function verse.plugins.ping(stream)
+	function stream:ping(jid, callback)
+		local t = socket.gettime();
+		stream:send_iq(verse.iq{ to = jid, type = "get" }:tag("ping", { xmlns = xmlns_ping }), 
+			function (reply)
+				callback(socket.gettime()-t, jid);
+			end);
+	end
+end
