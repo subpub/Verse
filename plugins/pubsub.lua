e8cd9086e83917f36e8ebeb2e86daa03dad3f4d2
Matthew Wild <mwild1@gmail.com>
2010-12-03 04:11:18 +0000

    plugins.pubsub: Fire pubsub/event on incoming pubsub notifications

--- plugins/pubsub.lua
+++ plugins/pubsub.lua
@@ -9,6 +9,21 @@ local pubsub_mt = { __index = pubsub };
 
 function verse.plugins.pubsub(stream)
 	stream.pubsub = setmetatable({ stream = stream }, pubsub_mt);
+	stream:hook("message", function (message)
+		for pubsub_event in message:matching_tags("event", xmlns_pubsub_event) do
+			local items = pubsub_event:get_child("items");
+			if items then
+				local node = items.attr.node;
+				for item in items:matching_tags("item") do
+					stream:event("pubsub/event", {
+						from = message.attr.from;
+						node = node;
+						item = item;
+					});
+				end
+			end
+		end
+	end);
 end
 
 function pubsub:subscribe(server, node, jid, callback)
