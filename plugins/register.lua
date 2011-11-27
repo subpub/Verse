2270ad761ba42c6ae1e5a0c2780714322ea78cd1
Matthew Wild <mwild1@gmail.com>
2011-11-27 21:56:21 +0000

    plugins.register: Add in-band registration plugin

---
+++ plugins/register.lua
@@ -0,0 +1,27 @@
+local xmlns_register = "jabber:iq:register";
+
+function verse.plugins.register(stream)
+	local function handle_features(features_stanza)
+		if features_stanza:get_child("register", "http://jabber.org/features/iq-register") then
+			stream:send_iq(verse.iq({ to = stream.host_, type = "set" })
+				:tag("query", { xmlns = xmlns_register })
+					:tag("username"):text(stream.username):up()
+					:tag("password"):text(stream.password):up()
+			, function (result)
+				if result.attr.type == "result" then
+					stream:event("registration-success");
+				else
+					local type, condition, text = result:get_error();
+					stream:debug("Registration failed: %s", condition);
+					stream:event("registration-failure", { type = type, condition = condition, text = text });
+				end
+			end);
+		else
+			stream:debug("In-band registration not offered by server");
+			stream:event("registration-failed", { condition = "service-unavailable" });
+		end
+		stream:unhook("stream-features", handle_features);
+		return true;
+	end
+	stream:hook("stream-features", handle_features, 310);
+end
