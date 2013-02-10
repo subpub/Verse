4949a68fa8960a1dbebf514e0d8925e03dc2b5d8
Kim Alvefur <zash@zash.se>
2013-02-10 04:20:44 +0100

    verse: Really pass the connection to ondisconnect, not some unknown global

--- init.lua
+++ init.lua
@@ -150,7 +150,7 @@ function stream:close(reason)
 	end
 	local on_disconnect = self.conn.disconnect();
 	self.conn:close();
-	on_disconnect(conn, reason);
+	on_disconnect(self.conn, reason);
 end
 
 -- Logging functions
